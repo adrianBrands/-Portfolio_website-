85384852fe639fb597d38cd4b439e920048e409a
adrianBrands <adrianbrandshaug@hotmail.com>
2022-10-23 14:20:57 +0200

    test url

--- js/index.js
+++ js/index.js
@@ -4,18 +4,6 @@ const videoUrl = "https://photos.google.com/share/AF1QipOMksnRrteer2XSySVRpyAiCP
 
 //"https://photos.google.com/share/AF1QipOMksnRrteer2XSySVRpyAiCPmIJwWeC2lXej7vkojdg0uy8NGWwHZq4b-OD22fZg/photo/AF1QipNCZ1hWDmiCSZ0xQgkMtFrhZ3QUfRT7GMUGpMj8?key=X0Q2QTNnajcyZkxkTHNLZXE4LWJUUzBUd2EwcEFR"
 //?key=X0Q2QTNnajcyZkxkTHNLZXE4LWJUUzBUd2EwcEFR?X0Q2QTNnajcyZkxkTHNLZXE4LWJUUzBUd2EwcEFR
-const options = { 
 
-  method: 'GET', 
-
-  headers: { 
-
-            'X-RapidAPI-Host': ' tasty.p.rapidapi.com', 
-
-            'X-RapidAPI-Key': '12345abcdef' 
-
-   } 
-
-};
 const corsEnabledUrl = "https://noroffcors.herokuapp.com/" + videoUrl;
-videoId.innerHTML = `<source src="${corsEnabledUrl}" type="video/mp4">`;
+//videoId.innerHTML = `<source src="${corsEnabledUrl}" type="video/mp4">`;
